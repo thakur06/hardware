5ae458cd0305cf384ae7ab025f2abe727f0458b6
ABHISHEK RAJPUT <79101220+thakur06@users.noreply.github.com>
2025-05-07 12:46:28 +0530

    added dsa and dev navigation

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ import {
 import { Projects } from "./pages/Projects";
 import { Blog } from "./pages/Blog";
 import { Personal } from "./pages/Personal";
+import { Dsa } from "./components/Dsa";
+import { Dev } from "./components/Dev";
 function Layout() {
   return (
     <div className="scrollbar-hide overflow-y-scroll h-screen">
@@ -36,7 +38,13 @@ function App() {
         { path: "/contact", element: <Contact /> },
         { path: "/projects", element: <Projects/> },
         { path: "/blog", element: <Blog/> },
-        { path: "/me", element: <Personal/> },
+        { path: "/me", element: <Personal/>,
+          children:[
+            { path: "/me", element: <Dsa/> },
+            { path: "/me/dev", element: <Dev/> },
+          ]
+         },
+       
       ],
     },
   ]);
---
+++ src/components/Dev.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Dev = () => {
+  return (
+    <div className='min-h-full p-5'>
+        <h1 className='text-2xl md:text-3xl' style={{fontFamily:"incognito"}}>Page is under construction . . .</h1>
+    </div>
+  )
+}
